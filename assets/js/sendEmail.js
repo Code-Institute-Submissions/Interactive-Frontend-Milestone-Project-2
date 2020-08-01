d9147948b7e7d1fe2d6b438b21623df904483e2e
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-08-01 15:24:20 +0000

    Fix Contact Page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("muthuvanitha_gmail_com", "vanitha", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "stockholm_tours": contactForm.Message.value
+    },"user_5KExuU3XUBpBZd0ZMo3JQ")
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
