b4e7c8696270e296d3a55ba19d33f6cb2ccad90b
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-08-02 08:40:26 +0000

    place search fix and map display fix in explore page

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,4 +1,4 @@
-
+/*
 function initMap() {
   // Create the map.
   const pyrmont = { lat: -33.866, lng: 151.196 };
@@ -6,8 +6,9 @@ function initMap() {
     center: pyrmont,
     zoom: 17
   });
+
   // Create the places service.
-  const service = new google.maps.places.PlacesService(map);
+    const service = new google.maps.places.PlacesService(map);
   let getNextPage;
   const moreButton = document.getElementById("more");
 
@@ -19,8 +20,8 @@ function initMap() {
     }
   };
   // Perform a nearby search.
-  service.nearbySearch(
-    { location: pyrmont, radius: 3500, type: "store" },
+service.nearbySearch(
+    { location: pyrmont, radius: 500, type: "store" },
     (results, status, pagination) => {
       if (status !== "OK") return;
       createMarkers(results, map);
@@ -57,4 +58,72 @@ function createMarkers(places, map) {
     bounds.extend(place.geometry.location);
   }
   map.fitBounds(bounds);
-}
+}*/
+function initMap() {
+        // Create the map.
+        const pyrmont = {
+          lat: -33.866,
+          lng: 151.196
+        };
+        const map = new google.maps.Map(document.getElementById("map"), {
+          center: pyrmont,
+          zoom: 17
+        }); // Create the places service.
+
+        const service = new google.maps.places.PlacesService(map);
+        let getNextPage;
+        var searchtype="store"
+        const moreButton = document.getElementById("more");
+
+        moreButton.onclick = function() {
+          moreButton.disabled = true;
+
+          if (getNextPage) {
+            getNextPage();
+          }
+        }; // Perform a nearby search.
+
+        service.nearbySearch(
+          {
+            location: pyrmont,
+            radius: 500,
+            type: searchtype
+          },
+          (results, status, pagination) => {
+            if (status !== "OK") return;
+            createMarkers(results, map);
+            moreButton.disabled = !pagination.hasNextPage;
+
+            if (pagination.hasNextPage) {
+              getNextPage = pagination.nextPage;
+            }
+          }
+        );
+      }
+
+      function createMarkers(places, map) {
+        const bounds = new google.maps.LatLngBounds();
+        const placesList = document.getElementById("places");
+
+        for (let i = 0, place; (place = places[i]); i++) {
+          const image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+          new google.maps.Marker({
+            map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+          });
+          const li = document.createElement("li");
+          li.textContent = place.name;
+          placesList.appendChild(li);
+          bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
+      }
