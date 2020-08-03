30f55873584a891404cf0b3d34afcd2476a325ee
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-08-03 11:53:07 +0000

    places api fix

--- assets/js/places.js
+++ assets/js/places.js
@@ -56,39 +56,60 @@
         map.fitBounds(bounds);
       }*/
 
-      let service;
+let service;
 let pyrmont;
 let map;
 function initMap() {
   // Create the map.
-   pyrmont = { lat: 59.32,lng: 18.06};
-   map = new google.maps.Map(document.getElementById("map"), {
+  pyrmont = { lat: 59.32, lng: 18.06 };
+  map = new google.maps.Map(document.getElementById("map"), {
     center: pyrmont,
-    zoom: 17
+    zoom: 12,
   });
   // Create the places service.
   service = new google.maps.places.PlacesService(map);
   let getNextPage;
 
   // Perform a nearby search.
-
-  
-  const selectElement = document.querySelectorAll('input[name="choice"]');
-  if (selectElement.checked) {
-    let loctype = selectElement.value;
-    console.log(loctype);
-    ns(loctype);
+  var choices = document.forms["choice"].elements["choice"];
+  for (var i = 0, max = choices.length; i < max; i++) {
+    choices[i].onclick = function () {
+      ns(this.value);
+    };
+  }
 }
 
-}
 function ns(loctype) {
   service.nearbySearch(
     { location: pyrmont, radius: 500, type: loctype },
     (results, status, pagination) => {
       if (status !== "OK") return;
-      createMarkers(results, map);
       
+      createMarkers(results, map);
     }
   );
 }
 
+function createMarkers(places, map) {
+ 
+  const bounds = new google.maps.LatLngBounds();
+  const placesList = document.getElementById("places");
+
+  for (let i = 0, place; (place = places[i]); i++) {
+    const image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    new google.maps.Marker({
+      map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+    bounds.extend(place.geometry.location);
+  }
+  map.fitBounds(bounds);
+}
